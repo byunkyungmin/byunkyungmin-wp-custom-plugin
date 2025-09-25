4de151735119bb4da1f7fdbf9f04ee5e0c0c57ac
Byun Kyung MIn <chgemn@gmail.com>
2025-09-25 20:15:08 +0900

    Update contacat html

--- contact-templates/ko/email-user.php
+++ contact-templates/ko/email-user.php
@@ -34,11 +34,9 @@
       margin-bottom: 12px;
       text-align: left; /* 본문은 다시 왼쪽 정렬 */
     }
-
     p {
-      text-align: left;
+      font-size: 14px;
     }
-
     .message-box {
       background: #f2f2f2;
       border-radius: 8px;
@@ -63,14 +61,12 @@
     <div class="logo">
       <img src="https://cms.byunkyungmin.work/wp-content/uploads/2024/02/png_long.png" alt="Byun Kyung Min Logo" width="200" />
     </div>
-    <h3>궁금증이든 농담이든, 다 잘 받았습니다</h2>
+    <h2>궁금증이든 농담이든, 다 잘 받았습니다</h2>
     <p><a href="<?= esc_url($url) ?>" target="_blank"><?= esc_html($pageTitle) ?></a>에 관심가져주셔서 감사해요.</p>
     <p>남겨주신 내용:</p>
     <div class="message-box"><?= nl2br(esc_html($message)) ?></div>
-    <p><strong>답변이 빨리 오지 않는다면, 오래오래 고민중인거에요. <br />조금만 기다려주세요.</strong></p>
-    <a href="https://music.apple.com/kr/playlist/%EC%9A%94%EC%A6%98-%EB%82%98%EC%9D%98-%EC%B5%9C%EC%95%A0%EA%B3%A1-%EB%AF%B9%EC%8A%A4/pl.pm-8e975ad16716041cc2a38d10923704c3">
-      <p>기다리시는 동안, 제가 요즘 즐겨듣는 노래 들어보실래요?</p>
-    </a>
+    <p>답변이 빨리 오지 않는다면, 오래오래 고민중인거에요. <br />조금만 기다려주세요.</p>
+    <p>기다리시는 동안, <a href="https://music.apple.com/kr/playlist/%EC%9A%94%EC%A6%98-%EB%82%98%EC%9D%98-%EC%B5%9C%EC%95%A0%EA%B3%A1-%EB%AF%B9%EC%8A%A4/pl.pm-8e975ad16716041cc2a38d10923704c3">제가 요즘 즐겨듣는 노래</a> 들어보실래요?</p>
     <div class="footer">이 메일은 자동이지만, 답장은 제가 직접 쓸 거예요. <br />(그것도 아주 열심히요!)</div>
   </div>
 </body>
